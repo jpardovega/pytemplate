b409083bec399513f9fec6965f8c8e137414c212
jpardovega <jpardovega@gmail.com>
2021-03-28 21:14:26 -0400

    Update dependencies

--- setup.py
+++ setup.py
@@ -15,72 +15,70 @@ from setuptools import setup
 
 def read(*names, **kwargs):
     with io.open(
-        join(dirname(__file__), *names),
-        encoding=kwargs.get('encoding', 'utf8')
+        join(dirname(__file__), *names), encoding=kwargs.get("encoding", "utf8")
     ) as fh:
         return fh.read()
 
 
 setup(
-    name='pytemplate',
+    name="pytemplate",
     use_scm_version={
-        'local_scheme': 'dirty-tag',
-        'write_to': 'src/pytemplate/_version.py',
-        'fallback_version': '0.0.0',
+        "local_scheme": "dirty-tag",
+        "write_to": "src/pytemplate/_version.py",
+        "fallback_version": "0.0.0",
     },
-    description='This is a template for python package using cookiecutter-pylibrary',
-    long_description='%s\n%s' % (
-        re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
-        re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))
+    description="This is a template for python package using cookiecutter-pylibrary",
+    long_description="%s\n%s"
+    % (
+        re.compile("^.. start-badges.*^.. end-badges", re.M | re.S).sub(
+            "", read("README.rst")
+        ),
+        re.sub(":[a-z]+:`~?(.*?)`", r"``\1``", read("CHANGELOG.rst")),
     ),
-    author='Javier PV',
-    author_email='testemail@gmail.com',
-    url='https://github.com/jpardovega/pytemplate',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    author="Javier PV",
+    author_email="testemail@gmail.com",
+    url="https://github.com/jpardovega/pytemplate",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Operating System :: Unix',
-        'Operating System :: POSIX',
-        'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Operating System :: Unix",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
         # uncomment if you test on these interpreters:
         # 'Programming Language :: Python :: Implementation :: IronPython',
         # 'Programming Language :: Python :: Implementation :: Jython',
         # 'Programming Language :: Python :: Implementation :: Stackless',
-        'Topic :: Utilities',
+        "Topic :: Utilities",
     ],
     project_urls={
-        'Documentation': 'https://pytemplate.readthedocs.io/',
-        'Changelog': 'https://pytemplate.readthedocs.io/en/latest/changelog.html',
-        'Issue Tracker': 'https://github.com/jpardovega/pytemplate/issues',
+        "Documentation": "https://pytemplate.readthedocs.io/",
+        "Changelog": "https://pytemplate.readthedocs.io/en/latest/changelog.html",
+        "Issue Tracker": "https://github.com/jpardovega/pytemplate/issues",
     },
     keywords=[
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
-    python_requires='>=3.6',
-    install_requires=[
-        # eg: 'aspectlib==1.1.1', 'six>=1.7',
-    ],
+    python_requires=">=3.6",
+    install_requires=["pandas", "numpy"],
     extras_require={
         # eg:
         #   'rst': ['docutils>=0.11'],
         #   ':python_version=="2.6"': ['argparse'],
     },
-    setup_requires=[
-        'setuptools_scm>=3.3.1',
-    ],
+    setup_requires=["setuptools_scm>=3.3.1",],
 )
