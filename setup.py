fab89af488b10c2b790de387fa078328d9c4d26e
jpardovega <jpardovega@gmail.com>
2021-03-29 21:36:51 -0400

    test

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     use_scm_version={
         "local_scheme": "dirty-tag",
         "write_to": "src/pytemplate/_version.py",
-        "fallback_version": "0.0.0",
+        "fallback_version": '0.0.0',
     },
     description="This is a template for python package using cookiecutter-pylibrary",
     long_description="%s\n%s"
