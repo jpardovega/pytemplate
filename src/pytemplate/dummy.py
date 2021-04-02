468894c07baf4b0d68532d507b1a3d832a28be60
jpardovega <jpardovega@gmail.com>
2021-04-01 22:18:20 -0400

    Minor updates

--- src/pytemplate/dummy.py
+++ src/pytemplate/dummy.py
@@ -8,6 +8,13 @@ def dummyfunc():
     Returns:
         int: Always returns one.
     """
-    print(1+1)
+    print(1 + 1)
     print(os.getcwd())
+    myCamelVar = 5
+    print(myCamelVar)
     return 1
+
+
+def myfunc(a, b, c, d):
+    print(a + b + c + d)
+    print("hello")
